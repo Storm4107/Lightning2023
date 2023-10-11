49670dfde94eab20f5a141261e1d202157404f96
Team Storm 4107 <team4107@gmail.com>
2023-10-11 16:04:29 -0500

    Was made to test skills on programming basic DcMotors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/take1.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name = "take1")
+public class take1 extends LinearOpMode {
+
+    private DcMotor front_left;
+
+    private DcMotor front_right;
+    private DcMotor back_left;
+    private DcMotor back_right;
+
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // REV 40:1  1120
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 2.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 1.0;
+    static final double TURN_SPEED = 0.5;
+
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+    @Override
+    public void runOpMode() {
+        front_left = hardwareMap.get(DcMotor.class, "front_left");
+        front_right = hardwareMap.get(DcMotor.class, "front_right");
+        back_left = hardwareMap.get(DcMotor.class, "back_left");
+        back_right = hardwareMap.get(DcMotor.class, "back_right");
+
+
+        // Put initialization blocks here.
+        front_left.setDirection(DcMotorSimple.Direction.FORWARD);
+        front_right.setDirection(DcMotorSimple.Direction.REVERSE);
+        back_left.setDirection(DcMotorSimple.Direction.FORWARD);
+        back_right.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+        //Wait for start button to be pressed
+        waitForStart();
+        if (opModeIsActive()) {
+
+            // Put run blocks here.
+            while (opModeIsActive()) {
+
+                back_right.setPower(gamepad1.right_stick_y);
+                front_right.setPower(gamepad1.right_stick_y);
+                front_left.setPower(gamepad1.left_stick_y);
+                back_left.setPower(gamepad1.left_stick_y);
+            }
+        }
+    }
+
+}
+
+
